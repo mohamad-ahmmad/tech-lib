9159c266675ba024a3021aa48f1119ebf7338098
Mohammad Ahmad <eng.2002.mohammad@gmail.com>
2023-09-08 11:57:44 +0300

    Get Active Borrowers ✅

---
+++ functions/fn_GetActiveBorrowers.sql
@@ -0,0 +1,15 @@
+
+CREATE FUNCTION fn_GetActiveBorrowers()
+RETURNS TABLE
+AS
+RETURN
+WITH BorrowersCounts AS
+(
+	SELECT BorrowerID ,count('*') AS Times 
+	FROM Loans 
+	WHERE DateReturned IS NULL
+	GROUP BY BorrowerID
+)
+SELECT * 
+FROM BorrowersCounts
+WHERE Times >= 2;
