99237df3d7fc107f02a013cc6a31f95516fc4fe0
Mohammad Ahmad <eng.2002.mohammad@gmail.com>
2023-09-09 12:52:10 +0300

    sp_AddNewBorrower ✅

---
+++ procedures/sp_AddNewBorrower.sql
@@ -0,0 +1,20 @@
+
+CREATE PROC sp_AddNewBorrower 
+	@fname NVARCHAR(64),
+	@lname NVARCHAR(64),
+	@email NVARCHAR(256),
+	@birthdate DATE,
+	@membershipDate Date
+AS
+BEGIN
+
+	IF EXISTS (SELECT '*' FROM BORROWERS WHERE Email = @email)
+	BEGIN
+		RAISERROR('The email already exists', 16, 1);
+	END
+	
+	INSERT INTO Borrowers (FName, LName, email, Birthdate, MembershipDate) VALUES (@fname, @lname, @email, @birthdate, @membershipDate);
+	
+END
+
+
